c93e02c3cdf978acea647ab61fd3ac9744503baa
Vineet Kumar Rai <vineetrai@outlook.com>
2025-09-20 11:59:44 +0530

    Initial Commit

---
+++ Ecom/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Ecom
+//
+//  Created by Vineet Rai on 20-Sep-25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Ecom/EcomApp.swift
@@ -0,0 +1,17 @@
+//
+//  EcomApp.swift
+//  Ecom
+//
+//  Created by Vineet Rai on 20-Sep-25.
+//
+
+import SwiftUI
+
+@main
+struct EcomApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
